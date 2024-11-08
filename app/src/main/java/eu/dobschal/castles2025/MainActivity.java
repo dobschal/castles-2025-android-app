b151c328e21269f0e8efc8dd58b80e82a1503a13
Sascha Dobschal <saschadobschal@macbookright--sascha.fritz.box>
2024-11-08 13:35:15 +0100

    inital commit

---
+++ app/src/main/java/eu/dobschal/castles2025/MainActivity.java
@@ -0,0 +1,37 @@
+package eu.dobschal.castles2025;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar supportActionBar = getSupportActionBar();
+        if(supportActionBar != null) {
+            supportActionBar.hide();
+        }
+
+        WebView.setWebContentsDebuggingEnabled(true);
+
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://castles.dobschal.eu");
+    }
+}
